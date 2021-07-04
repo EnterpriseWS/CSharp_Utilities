d4661fd6c8a74acd1cf95dcdf1740a4a01c86e71
Robert Wang <robert.wang@enterprisews.com>
2021-07-03 17:40:16 -0700

    Initial utilities

---
+++ UtilityConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UtilityConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ReverString("My God!"));
+            Console.WriteLine(ReverseInteger(-123));
+        }
+
+        static string ReverString(string theString)
+        {
+            string rtnString = "";
+            for (int idx = theString.Length-1; idx >= 0; idx--)
+                rtnString += theString[idx];
+
+            return rtnString;
+        }
+
+        static int ReverseInteger(int theInteger)
+        {
+            string theIntegerStr = theInteger < 0? (-(theInteger)).ToString(): theInteger.ToString();
+            
+            return theInteger<0? -(Convert.ToInt32(ReverString(theIntegerStr))): Convert.ToInt32(ReverString(theIntegerStr));
+        }
+    }
+}
